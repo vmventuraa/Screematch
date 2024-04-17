2d417b79f23da75964426c2aa99d49aba305a72b
Victor Ventura <vmventura025@gmail.com>
2024-04-17 19:13:59 -0300

    Utilizacao de lambdas para listar episodios

--- src/main/java/br/com/alura/Screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/Screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.alura.Screenmatch.principal;
 
+import br.com.alura.Screenmatch.model.DadosEpisodios;
 import br.com.alura.Screenmatch.model.DadosSerie;
 import br.com.alura.Screenmatch.model.DadosTemporada;
 import br.com.alura.Screenmatch.service.ConsumoAPI;
@@ -11,13 +12,14 @@ import java.util.Scanner;
 
 
 public class Principal {
+    //Constantes do link de consumo da API
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String APIK_KEY = "&apikey=406855ed";
+
     private Scanner leitura = new Scanner(System.in);
 
     private ConsumoAPI consumoAPI = new ConsumoAPI();
-    ConverteDados conversor = new ConverteDados();
-
-    private String ENDERECO = "https://www.omdbapi.com/?t=";
-    private String APIK_KEY = "&apikey=406855ed";
+    private ConverteDados conversor = new ConverteDados();
 
 
     public void exibeMenu() {
@@ -39,6 +41,16 @@ public class Principal {
 
         }
         temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodios> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//
+//        }
+
+        temporadas.forEach(t->t.episodios().forEach(e -> System.out.println(e.titulo())));
 
 
     }
