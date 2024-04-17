5cc3db7fd6ae994f155644b5fcd4c8d379f6e2b9
Victor Ventura <vmventura025@gmail.com>
2024-04-17 18:03:56 -0300

    Criacao dos dados de episodios por temporada

--- src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package br.com.alura.Screenmatch;
 
 import br.com.alura.Screenmatch.model.DadosEpisodios;
 import br.com.alura.Screenmatch.model.DadosSerie;
+import br.com.alura.Screenmatch.model.DadosTemporada;
 import br.com.alura.Screenmatch.service.ConsumoAPI;
 import br.com.alura.Screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -22,13 +26,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
         var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=breaking+bad&apikey=406855ed");
 
 
-        System.out.println(json);
+//        System.out.println(json);
         ConverteDados conversor = new ConverteDados();
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 
-
+//
         System.out.println();
-        System.out.println(dados);
+//        System.out.println(dados);
 
         json = consumoAPI.obterDados("https://www.omdbapi.com/?t=breaking+bad&season=5&episode=14&apikey=406855ed");
 
@@ -36,8 +40,17 @@ public class ScreenmatchApplication implements CommandLineRunner {
         DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
 
 
-        System.out.println(dadosEpisodios);
+//        System.out.println(dadosEpisodios);
 
+        List<DadosTemporada> temporadas = new ArrayList<>();
+       for (int i =1; i <= dados.totalTemporadas();i++){
+           json = consumoAPI.obterDados("https://www.omdbapi.com/?t=peaky+blinders&season="+i+"&apikey=406855ed");
+           DadosTemporada dadosTemporada = conversor.obterDados(json,DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+
+
+       }
+       temporadas.forEach(System.out::println);
 
 
 
--- src/main/java/br/com/alura/Screenmatch/model/DadosTemporada.java
+++ src/main/java/br/com/alura/Screenmatch/model/DadosTemporada.java
@@ -1,8 +1,12 @@
 package br.com.alura.Screenmatch.model;
 
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosTemporada() {
+public record DadosTemporada(@JsonAlias("Season") Integer numero,
+                             @JsonAlias("Episodes") List<DadosEpisodios> episodios) {
 }
