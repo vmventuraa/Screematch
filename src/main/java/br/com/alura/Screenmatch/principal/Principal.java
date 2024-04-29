7e16d712853a13471d52602fc792a85fdbe3df3b
Victor Ventura <vmventura025@gmail.com>
2024-04-29 18:50:41 -0300

    Aplicação de estatísticas

--- src/main/java/br/com/alura/Screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/Screenmatch/principal/Principal.java
@@ -81,19 +81,19 @@ public class Principal {
 
         episodios.forEach(System.out::println);
 
-        System.out.println("Informe um episódio para busca");
-        var trechoTitulo = leitura.nextLine();
-        Optional<Episodio> episodioBuscado = episodios.stream()
-                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
-                .findFirst();
-
-        if (episodioBuscado.isPresent()) {
-            System.out.println("Episódio encontrado");
-            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
-            System.out.println("Titulo: " + episodioBuscado.get().getTitulo());
-        }else {
-            System.out.println("Episodio não encontrado");
-        }
+//        System.out.println("Informe um episódio para busca");
+//        var trechoTitulo = leitura.nextLine();
+//        Optional<Episodio> episodioBuscado = episodios.stream()
+//                .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+//                .findFirst();
+//
+//        if (episodioBuscado.isPresent()) {
+//            System.out.println("Episódio encontrado");
+//            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+//            System.out.println("Titulo: " + episodioBuscado.get().getTitulo());
+//        }else {
+//            System.out.println("Episodio não encontrado");
+//        }
 
 
 //        System.out.println("A partir de que ano você deseja ver os episódios? ");
@@ -120,6 +120,16 @@ public class Principal {
 
         System.out.println(avaliacoesPorTemporada);
 
+    DoubleSummaryStatistics est = episodios.stream()
+            .filter(e -> e.getAvaliacao() > 0.0)
+            .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+
+        System.out.println();
+        System.out.println("Média:" + est.getAverage());
+        System.out.println();
+        System.out.println("Melhor Episódio:" + est.getMax());
+        System.out.println("Pior Episódio:" + est.getMin());
+        System.out.println("Quantidade:" + est.getCount());
 
 
 
